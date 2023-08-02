e3c2e219bc21fa6e90c706168f5a7854d4bf5065
Yaswanth Mareedu <yaswanthyashu333@gmail.com>
2023-08-02 13:50:32 -0400

    database connectivity changed

--- app/database.py
+++ app/database.py
@@ -30,7 +30,7 @@ def get_db():
 while True:
 
     try:
-        conn = psycopg2.connect(host='localhost',database='fastapi',user='postgres',password='yash1234',cursor_factory=RealDictCursor)
+        conn = psycopg2.connect(host=settings.database_hostname,database=settings.database_name,user=settings.database_username,password=settings.database_password,cursor_factory=RealDictCursor)
         print("Connection successful")
         cursor = conn.cursor()
         break
